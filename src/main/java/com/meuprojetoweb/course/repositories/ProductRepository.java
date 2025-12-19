883efbc91a47ac81a461296bfc6ec820423e264b
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2025-12-18 22:09:34 -0300

    Product

---
+++ src/main/java/com/meuprojetoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.meuprojetoweb.course.repositories;
+
+import com.meuprojetoweb.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
